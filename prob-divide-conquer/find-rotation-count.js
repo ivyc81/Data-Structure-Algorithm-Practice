20d814c1716c1ff4d8748d7aded1a57da5bbeb19
Cynthia <ivyc8192@gmail.com>
2019-06-03 19:38:29 -0700

    practice divide-conquer

--- prob-divide-conquer/find-rotation-count.js
+++ prob-divide-conquer/find-rotation-count.js
@@ -1,5 +1,19 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while(end >= start){
+    let mid = Math.floor((end + start) / 2);
+    if(arr[mid] < arr[mid - 1]){
+      return mid;
+    } else if(arr[mid] >= arr[start]){
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return 0;
 }
 
 module.exports = findRotationCount
--- prob-divide-conquer/sorted-frequency.js
+++ prob-divide-conquer/sorted-frequency.js
@@ -1,5 +1,47 @@
-function sortedFrequency() {
+function sortedFrequency(arr, targetNum) {
+  const start = arr[0] === targetNum ? 0 : findSmaller(arr, targetNum);
+  const end = arr[arr.length - 1] === targetNum ? arr.length : findLarger(arr, targetNum);
 
+  if(end === -1 || start === -1){
+    return -1;
+  }
+  return end - start;
+}
+
+function findSmaller(arr, targetNum){
+  let start = 0;
+  let end = arr.length - 1;
+
+  while(end >= start){
+    let mid = Math.floor((end + start) / 2);
+    if(arr[mid] === targetNum && arr[mid - 1] < arr[mid]){
+      return mid;
+    } else if (arr[mid] >= targetNum){
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return -1;
+}
+
+function findLarger(arr, targetNum){
+  let start = 0;
+  let end = arr.length - 1;
+
+  while(end >= start){
+    let mid = Math.floor((end + start) / 2);
+    if(arr[mid] === targetNum && arr[mid + 1] > arr[mid]){
+      return mid + 1;
+    } else if (arr[mid] > targetNum){
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return -1;
 }
 
 module.exports = sortedFrequency
