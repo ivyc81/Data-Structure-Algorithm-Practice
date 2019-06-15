3fc1d1cb9d8b5e771b446d9efc85f740dc18053b
Cynthia <ivyc8192@gmail.com>
2019-06-15 16:32:41 -0700

    find-longest-substring

--- prob-sliding-window/find-longest-substring.js
+++ prob-sliding-window/find-longest-substring.js
@@ -1,4 +1,23 @@
 // add whatever parameters you deem necessary
-function findLongestSubstring() {}
+function findLongestSubstring(str) {
+  let longest = 0;
+  let start = 0;
+  let seen = {};
+
+  for(let i = 0; i < str.length; i++){
+    if(seen[str[i]] !== undefined){
+      longest = Math.max(longest, i - start);
+      start = seen[str[i]] + 1;
+      i = seen[str[i]];
+      seen = {};
+    } else if(i === str.length - 1){
+      longest = Math.max(longest, i - start + 1);
+    } else {
+      seen[str[i]] = i;
+    }
+  }
+
+  return longest;
+}
 
 module.exports = findLongestSubstring;
