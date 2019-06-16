fab9c14f7da113211b4fc216d4a549e880148d63
Cynthia <ivyc8192@gmail.com>
2019-06-15 21:37:35 -0700

    min-subarray-length

--- prob-sliding-window/min-subarray-length.js
+++ prob-sliding-window/min-subarray-length.js
@@ -1,4 +1,27 @@
 // add whatever parameters you deem necessary
-function minSubarrayLength() {}
+function minSubarrayLength(arr, target) {
+  let i = 0;
+  let j = -1;
+  let sum = 0;
+  let minLength = Infinity;
+
+  while(j < arr.length && (i < arr.length || sum > target)){
+    if(sum < target){
+      j++
+      sum += arr[j];
+    }
+    if(sum >= target){
+      minLength = Math.min(minLength, j - i + 1);
+      sum -= arr[i];
+      i++
+    }
+  }
+
+  if(j === arr.length && i === 0){
+    return 0;
+  }
+
+  return minLength;
+}
 
 module.exports = minSubarrayLength;
